d1a45a7dc526d7c86d2aa74ddef34a0156a0468c
Evan022823 <evanjustine.clavecilla.g@bulsu.edu.ph>
2024-06-08 23:01:11 +0800

    auth

---
+++ registration.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        <label for="user_name">User name:</label><br>
+        <input type="text" id="Username" name="username"><br>
+        <label for="email_add">Email:</label><br>
+        <input type="email" id="email" name="email"> <br>
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password"> <br>
+        <label for="confirm">Re-type Password:</label><br>
+        <input type="password" id="confirm" name="confirmpassword"></br><br>
+        <button type= "submit" name="submit" >Register</button>
+    </form>
+
+    <a href="login.php">LogIn</a>
+
+</body>
+</html>
